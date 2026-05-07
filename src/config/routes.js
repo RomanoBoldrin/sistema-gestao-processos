d07dc63434b16e05b56248bbef02ffe0b8b7c4d8
RomanoBoldrin <107365022+RomanoBoldrin@users.noreply.github.com>
2026-05-07 10:47:50 -0300

    chore(config): add application configuration files

---
+++ src/config/routes.js
@@ -0,0 +1,32 @@
+export const routes = [
+  {
+    label: "Dashboard",
+    path: "/",
+    key: "dashboard",
+  },
+  {
+    label: "Cases",
+    path: "/cases",
+    key: "cases",
+  },
+  {
+    label: "Calendar",
+    path: "/calendar",
+    key: "calendar",
+  },
+  {
+    label: "Clients",
+    path: "/clients",
+    key: "clients",
+  },
+  {
+    label: "Billing",
+    path: "/billing",
+    key: "billing",
+  },
+  {
+    label: "Documents",
+    path: "/documents",
+    key: "documents",
+  },
+];
