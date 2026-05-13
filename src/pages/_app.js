634df6a6ebb683655afa4d2a4a9f3f8c6e7700bb
RomanoBoldrin <107365022+RomanoBoldrin@users.noreply.github.com>
2026-05-13 20:09:35 -0300

    feat(layout): add conditional layout rendering for login page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,18 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
 
 import { RootLayout } from "@/components/layout/RootLayout";
 
+const NO_LAYOUT_PAGES = ["/login"];
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const showLayout = !NO_LAYOUT_PAGES.includes(router.pathname);
+
+  if (!showLayout) {
+    return <Component {...pageProps} />;
+  }
+
   return (
     <RootLayout>
       <Component {...pageProps} />
